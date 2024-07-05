6a3fbd2a53275cfe3f36faca64d0adaad6f6311b
Aakash Bandhu Aryal <94972492+AakashHelloworld@users.noreply.github.com>
2024-07-05 21:53:26 +0545

    git push

--- src/app/Player/_componenets/Navbar.tsx
+++ src/app/Player/_componenets/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     const router = useRouter();
 
     return (
-        <nav className='flex h-[50px] bg-white  border-b border-gray-200 justify-end p-4 nav m-4 '>
+        <nav className='flex bg-white  border-b border-gray-200 justify-end items-center px-4 py-2 nav  '>
             <Button onClick={() => router.push('/pdf')}>ALL PDF</Button>
         </nav>
     )
